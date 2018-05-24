6092f8309823ad34a4177035f28ef0e973419207
DanhNQ <danhnq7@gmail.com>
2018-05-24 16:49:42 +0700

    Background + Controll + Collision Basic

---
+++ Assets/Scripts/BackGround.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : SomeThing {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	new void Destroy()
+    {
+        if (this.transform.position.z <= Variables.minZ)
+        {
+            Vector3 pos = this.transform.position;
+            pos.z = Variables.maxBGZ;
+            this.transform.position = pos;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        Update_frame();
+        Destroy();
+	}
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player");
+        Debug.Log(other.name);
+        if (other.tag == "obstacle")
+        {
+            Debug.Log("Trung cmnr");
+            this.GetComponent<Rigidbody>().velocity = new Vector3(0f, 5f, 0f);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
